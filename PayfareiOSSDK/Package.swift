6fefe0bdaa0519e5c34c0f0a5ee67a47dda52acb
kkolontai <kkolontai@payfare.com>
2023-03-02 22:29:46 -0500

    last

--- PayfareiOSSDK/Package.swift
+++ PayfareiOSSDK/Package.swift
@@ -16,11 +16,12 @@ let package = Package(
     dependencies: [
     ],
     targets: [
+        .target(name: "PayfareiOSSDK"),
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "PayfareiOSSDK",
-            dependencies: [.byName(name: "PayfareiSDK")]),
+//        .target(
+//            name: "PayfareiOSSDK",
+//            dependencies: [.byName(name: "PayfareiSDK")]),
         .testTarget(
             name: "PayfareiOSSDKTests",
             dependencies: ["PayfareiOSSDK"]),
